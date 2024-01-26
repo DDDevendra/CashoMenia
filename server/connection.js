424f75aa91931795d2501d7e1a44f1efd227b4ad
Devendra <devdangare88@gmail.com0>
2024-01-26 22:28:22 +0530

    Remove node_modules from tracking

---
+++ server/connection.js
@@ -0,0 +1,21 @@
+import mongoose, { connect } from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+async function Connect(){
+    console.log(process.env.DB_URL);
+
+    const ab = await mongoose.connect(process.env.DB_URL, {
+      }).then(() => {
+  
+        console.log("DataBase is connected !");
+  
+      }).catch((error) => {
+  
+        console.log(error);
+  
+      });
+}
+
+export default Connect;
---
+++ server/server.js
@@ -0,0 +1,29 @@
+import  express  from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import Connect from "./connection.js";
+
+const app  = express();
+const PORT = process.env.PORT || 9000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', ()=>{
+    console.log("Server is Connected !");
+})
+
+
+
+Connect().then(()=>{
+
+    app.listen(PORT, async()=>{
+        console.log("server is Connected at " + PORT);
+    })
+    
+}).catch((error)=>{
+        console.log("Faild To Connect server !");
+})
