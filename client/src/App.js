c479091d5d714fda64901da1b39feaf10b9ad4e6
Devendra <devdangare88@gmail.com0>
2024-01-26 21:40:39 +0530

    Client SetUP 1

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './pages/Home';
+
+import './App.css';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/Signup" element={<Signup/>}/>
+          <Route path="/Home" element={<Home/>}/>
+      </Routes>
+     </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
